0c068d94424a5fd19f65c7938bc1371f6cd94643
Jesse Chen <jesse.z.chen@gmail.com>
2020-02-24 11:59:37 -0500

    Orthographic projection

--- giga.js
+++ giga.js
@@ -55,72 +55,70 @@ class Point {
 }
 
 const HEX_SIZE = 50;
+const HEX_HEIGHT_MULTIPLIER = 100;
 const HEX_RADIUS = 5;
 const ARRAY_SIZE = HEX_RADIUS * 2 + 1;
 const STEP_SIZE = 0.005;
 const MAX_DEPTH = 5;
-const numberFormat = Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 2,
-});
 let canvasWidth, canvasHeight;
 let biases;
 let depths;
 
 function draw() {
     background(240);
-    const centerWidth = canvasWidth / 2;
-    const centerHeight = canvasHeight / 2;
+    noStroke();
+    ambientLight(127);
+    pointLight(255, 255, 255, 500, -500, 1200);
     for (let q = -HEX_RADIUS; q <= HEX_RADIUS; q++) {
         for (let r = -HEX_RADIUS; r <= HEX_RADIUS; r++) {
             const point = new Point(q, r);
-            if (point.inBounds())  {
+            if (point.inBounds()) {
                 updateDepth(point);
-                drawHexagon(
-                    centerWidth + (3 / 2 * HEX_SIZE * q),
-                    centerHeight + (HEX_SIZE * sqrt(3) / 2 * (2 * r + q)),
+                drawPrism(
+                    3 / 2 * HEX_SIZE * q,
+                    HEX_SIZE * sqrt(3) / 2 * (2 * r + q),
                     point);
             }
         }
     }
 }
 
+function mousePressed() {
+    loop();
+}
+
+function mouseReleased() {
+    noLoop();
+}
+
 function setup() {
     canvasWidth = windowWidth;
     canvasHeight = windowHeight;
-    createCanvas(canvasWidth, canvasHeight);
+    createCanvas(canvasWidth, canvasHeight, WEBGL);
     background(240);
+    const bounds = HEX_SIZE * 2 * HEX_RADIUS;
+    ortho(-bounds, bounds, -bounds + HEX_HEIGHT_MULTIPLIER, bounds, + HEX_HEIGHT_MULTIPLIER);
     biases = Array.from(Array(ARRAY_SIZE),
         () => Array.from(Array(ARRAY_SIZE),
             () => randomGaussian()));
     depths = Array.from(Array(ARRAY_SIZE),
         () => Array.from(Array(ARRAY_SIZE),
             () => randomGaussian()));
+    noLoop();
 }
 
-function drawHexagon(x, y, point) {
-    const height = Math.sqrt(3) * HEX_SIZE;
-    const width = 2 * HEX_SIZE;
-    stroke(0, 15, 85);
-    if (Math.abs(mouseX - x) < width / 2 &&
-        Math.abs(mouseY - y) < height / 2) {
-        point.setDepth(2.5);
+function drawPrism(x, y, point) {
+    const depth = (point.getDepth() + MAX_DEPTH / 2) * HEX_HEIGHT_MULTIPLIER;
+    if (depth > 0) {
+        push();
+        rotateX(TAU / 4);
+        rotateX(TAU / 12);
+        rotateY(-TAU / 12);
+        translate(y, depth / 2, x);
+        fill(150);
+        cylinder(HEX_SIZE, depth, 6);
+        pop();
     }
-    noFill();
-    beginShape();
-    vertex(x - 1/4 * width, y - height / 2);
-    vertex(x + 1/4 * width, y - height / 2);
-    vertex(x + 1/2 * width, y);
-    vertex(x + 1/4 * width, y + height / 2);
-    vertex(x - 1/4 * width, y + height / 2);
-    vertex(x - 1/2 * width, y);
-    endShape(CLOSE);
-    textAlign(CENTER, CENTER);
-    textSize(24);
-    fill(80, 0, 80);
-    text(numberFormat.format(point.getDepth()), x, y - 15);
-    textSize(16);
-    fill(0, 15, 85);
-    text(numberFormat.format(point.getBias()), x, y + 20);
 }
 
 function updateDepth(point) {
